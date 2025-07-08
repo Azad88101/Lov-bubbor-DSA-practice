c628952498f18691da6487e05266fa1937d2970e
azad88101 <ayushrawat88101@gmail.com>
2025-07-08 22:50:48 +0530

    Remove unused node instantiation code from temporary file

--- 1 Cpp_By_Lov_bubbor/12 Linked_List/LL Class 1/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
- // Node *first = new Node();
-    // Node *second = new Node();
-    // Node *third = new Node();
-    // Node *four = new Node();
-    // Node *five = new Node();
-    // Node *six = new Node();
