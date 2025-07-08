482f6e0e8458af02557f0dce6942e3fd48eebee0
azad88101 <ayushrawat88101@gmail.com>
2025-07-08 15:23:55 +0530

    Refactor Node class constructor to accept data parameter and update insertion functions for clarity

--- 1 Cpp_By_Lov_bubbor/12 Linked_List/LL Class 1/intro.cpp
+++ 1 Cpp_By_Lov_bubbor/12 Linked_List/LL Class 1/intro.cpp
@@ -7,9 +7,9 @@ class Node
 public:
     int data;
     Node *next;
-    Node()
+    Node(int d)
     {
-        data = 0;
+        data = d;
         next = NULL;
 
         // cout << "default con";
@@ -27,25 +27,100 @@ public:
         cout << "desctructor";
     }
 };
+
+void Print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << "  " << temp->data;
+        temp = temp->next;
+    }
+}
+
+void InsertNodeBeforHead(Node *&head, int data)
+{
+    if (head == NULL)
+    {
+        head = new Node(data);
+        return;
+    }
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+// void InsertNodeAfterHead(Node *&tail, int data)
+// {
+//     if (tail == NULL)
+//     {
+//         tail = new Node(data);
+//         return;
+//     }
+//     Node *newNode = new Node(data);
+//     // newNode->next = tail->next;
+//     tail->next = newNode;
+//     tail = newNode;
+// }
+
+
+
+void InsertNodeAfterHead(Node *&head, int data)
+{
+    if (head == NULL)
+    {
+        head = new Node(data);
+        return;
+    }
+    Node *newNode = new Node(data);
+    newNode->next = head->next;
+    head->next = newNode;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
-    Node *a = new Node;
-    Node *b = new Node;
-    Node *c = new Node;
-    Node *d = new Node;
-    Node *e = new Node;
+    //     Node *a = new Node(10);
+    //     Node *b = new Node(20);
+    //     Node *c = new Node(30);
+    //     Node *d = new Node(40);
+    //     Node *e = new Node(50);
 
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    d->next = e;
-    a->getValue();
-    b->getValue();
-    c->getValue();
-    d->getValue();
+    //     a->next = b;
+    //     b->next = c;
+    //     c->next = d;
+    //     d->next = e;
+    // a->getValue();
+    // b->getValue();
+    // c->getValue();
+    // d->getValue();
     // cout << e;
 
-    delete a;
+    // delete a;
+
+    // Print(a);
+
+    // HOw to insert node//
+
+    // Node *head = new Node(10);
+    // Node *head = NULL;
+    // InsertNodeBeforHead(head, 12);
+    // InsertNodeBeforHead(head, 17);
+    // InsertNodeBeforHead(head, 154);
+    // InsertNodeBeforHead(head, 15976);
+    // InsertNodeBeforHead(head, 186);
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    
+    // InsertNodeAfterHead(head, 12);
+    // InsertNodeAfterHead(head, 17);
+    // InsertNodeAfterHead(head, 154);
+    // InsertNodeAfterHead(head, 15976);
+    // InsertNodeAfterHead(head, 186);
+    Print(head);
 
     return 0;
 }
