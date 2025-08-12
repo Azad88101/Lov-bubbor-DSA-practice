da0ea27208850bfe6450bc4d1690501d52071c38
azad88101 <ayushrawat88101@gmail.com>
2025-08-12 15:47:59 +0530

    Refactor queue class debug messages for clarity and consistency

--- 1 Cpp_By_Lov_bubbor/14 Queue/Class 1/2impl_with_zero.cpp
+++ 1 Cpp_By_Lov_bubbor/14 Queue/Class 1/2impl_with_zero.cpp
@@ -16,43 +16,51 @@ public:
         arr = new int[size];
         front = 0;
         rear = 0;
+        cout << "[DEBUG] Queue created with size " << size << "\n";
     }
+
     void push(int data)
     {
+        cout << "[DEBUG] Trying to push " << data << "...\n";
         if (rear >= size)
         {
-            cout << " Q is FUll\n";
+            cout << "[DEBUG] Queue is FULL. Cannot insert " << data << "\n";
             return;
         }
         arr[rear] = data;
         rear++;
+        cout << "[DEBUG] Inserted " << data << " at position " << (rear - 1)
+             << ". Front: " << front << ", Rear: " << rear << "\n";
     }
 
     void pop()
     {
-
+        cout << "[DEBUG] Trying to pop...\n";
         if (rear == front)
         {
-            cout << " Q is EMPTY \n";
+            cout << "[DEBUG] Queue is EMPTY. Cannot pop.\n";
             return;
         }
+        cout << "[DEBUG] Removed element " << arr[front] << " from position " << front << "\n";
         front++;
+        cout << "[DEBUG] Front moved to " << front << ", Rear: " << rear << "\n";
     }
 
     int getfront()
     {
-
+        cout << "[DEBUG] Getting front element...\n";
         if (rear == front)
         {
-            cout << " Q is EMPTY \n";
+            cout << "[DEBUG] Queue is EMPTY.\n";
             return -1;
         }
+        cout << "[DEBUG] Front element is " << arr[front] << "\n";
         return arr[front];
     }
 };
-int main(int argc, char const *argv[])
-{
 
+int main()
+{
     Q q(3);
 
     q.pop();
@@ -60,6 +68,10 @@ int main(int argc, char const *argv[])
     q.push(2);
     q.push(3);
     q.push(4);
+
+    q.getfront();
+    q.pop();
+    q.getfront();
 
     return 0;
 }
