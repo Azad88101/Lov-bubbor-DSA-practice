9dc8c4bee395aee12d975f725a646d658a368531
azad88101 <ayushrawat88101@gmail.com>
2025-09-14 19:07:21 +0530

    Implement linked list with insertion, deletion, and print functionality in C++

---
+++ 1 Cpp_By_Lov_bubbor/12 Linked_List/LL Class 1/001_intro.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+// #include <iostream>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insert(node *&head, int data)
+{
+    node *t = new node(data);
+    if (head == NULL)
+    {
+        head = t;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = t;
+    t->next = NULL;
+}
+
+void printLL(node *head)
+{
+    node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+void deletion(node *&head, int pos)
+{
+    if (!head || pos <= 0)
+        return;
+    node *curr = head;
+    if (pos == 1)
+    {
+        head = curr->next;
+        curr->next = NULL;
+        delete curr;
+        return;
+    }
+
+    node *prev = NULL;
+    int count = 1;
+    while (count < pos && curr->next)
+    {
+        count++;
+        prev = curr;
+        curr = curr->next;
+    }
+    if (!curr->next)
+        return;
+
+    prev->next = curr->next;
+    delete curr;
+}
+
+int main(int argc, char const *argv[])
+{
+    node *head = NULL;
+    insert(head, 2);
+    insert(head, 1);
+
+    insert(head, 5);
+    insert(head, 8);
+    insert(head, 1);
+
+    insert(head, 5);
+    insert(head, 8);
+    insert(head, 1);
+
+    insert(head, 5);
+    insert(head, 8);
+
+    printLL(head);
+
+    deletion(head, 1);
+
+    printLL(head);
+
+    return 0;
+}
